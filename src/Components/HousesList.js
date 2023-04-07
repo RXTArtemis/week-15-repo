ec842689ee635496cea7035cbc47f41f8514df2f
RXTArtemis <r_nava20@outlook.com>
2023-04-06 19:59:54 -0500

    changes

--- src/Components/HousesList.js
+++ src/Components/HousesList.js
@@ -4,64 +4,54 @@ import { housesApi } from "../rest/HousesApi.js";
 import {useEffect, useState} from 'react';
 
 
-// const housesName=["Meadow Manor, Party House"];
-// const houses=[];
-// export class HousesList extends React.Component{
-    
-//     constructor (props){
-//         super(props)
-//         this.state = {
-//             houses:['Meadow Manor', 'Yellow House'],
-           
-//         };
 
-      
-//     }
+const houses={house1Name:'Meadow Manor', house2Name:'Red House'};
+const houseProperties= Object.values(houses);
+console.log(houseProperties);
+
+export class HousesList extends React.Component{
+    constructor (props){
+        super(props)
+        this.state = {
+          houses:[]
+        };
+    }
 
    
-export function HousesList(){
-    const[house, setHouseData]=useState(['Meadow Manor', "Yellow House"]);
-
-    useEffect(()=>{
-        getData();
-    },[]);
-    
-    const getData =async()=>{
-        const response =  await fetch('https://642e2b578ca0fe3352ca1028.mockapi.io/Houses-API')
-        .then((response)=>response.json());
-        setHouseData(response);
-    }
-    
-        // componentDidMount(){
-        //     this.fetchHouses();
-        // };
-    
-        let fetchHouses = async()=>{
-            const houses= await housesApi.get();
-            this.setState({houses});
-            console.log(houses);
-        };
-    
-        let updateHouse = async (updatedHouse) => {
-            await housesApi.put(updatedHouse);
-            this.fetchHouses();
-        }
-    
-            return(
-                <div className="house-list" >
-                    {house && house.map((house)=>(
-                        <House
-                        house={house}
-                        key={house.id}
-                        updateHouse={this.updateHouse}
-                        />
-                    ))}
-                </div>
-            )
-         }
-    
-
-
-
-
 //change to useEffect
+// useEffect(()=>{
+//     getData();
+// },[]);
+
+    componentDidMount(){
+        this.fetchHouses();
+    };
+
+    fetchHouses = async()=>{
+        const houses= await housesApi.get();
+        this.setState({houses});
+
+    };
+
+    updateHouse = async (updatedHouse) => {
+        await housesApi.put(updatedHouse);
+        this.fetchHouses();
+    };
+
+    render(){
+        return(
+            <div className="house-list" >
+                {houseProperties.map((house)=>(
+                    
+                    <House
+                    house={house}
+                    houseName={this.houseName}
+                    key={house.id}
+                    updateHouse={this.updateHouse}
+                    />
+                ))}
+            </div>
+        )
+    }
+}
+
